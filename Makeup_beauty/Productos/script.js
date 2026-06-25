771c5740b31fb639fdcd3eba812136d1a3f5faca
ChristianSV <krisycaritomio@gmail.com>
2026-06-25 10:28:05 -0600

    Delete Makeup_beauty directory

--- Makeup_beauty/Productos/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-    const banner = document.querySelector(".banner");
-
-    if (window.scrollY > 100) {
-        banner.classList.add("pequeno");
-    } else {
-        banner.classList.remove("pequeno");
-    }
-});
